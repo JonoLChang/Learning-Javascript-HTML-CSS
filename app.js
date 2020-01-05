324fb5099855d6dd040bf90b0d2e4cfebbefc953
Jonathan Chang <jonoleighchang@gmail.com>
2020-01-05 12:28:30 -0400

    third commit

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-const age = 22 ; 
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false
+    }
+]; 
 
-age = 23
-console.log(age); 
+console.log(todos[1].text) ;
